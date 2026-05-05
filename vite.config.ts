00e581d99d3b2a68e8daa24cd2e4c11acee29ac7
Houssein372021 <hseinghannoum@gmail.com>
2026-05-05 17:36:52 +0200

    fix vercel base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: "/houssein_Portfolio/",
+  base: "/",
   plugins: [react(), tailwindcss(), tsconfigPaths()],
 });
